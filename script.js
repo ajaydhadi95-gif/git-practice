39b224538608ab6c357f8bbfbe83e404e5836945
ajaydhadi95-gif <ajaydhadi95@gmail.com>
2026-04-28 11:19:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-// HTML elements ko select karna
-const btn = document.getElementById('modeBtn');
 
-// Click event add karna
-btn.addEventListener('click', () => {
-    // Body par 'dark-theme' class ko toggle (add/remove) karna
-    document.body.classList.toggle('dark-theme');
-    
-    // Button ka text badalna
-    if (document.body.classList.contains('dark-theme')) {
-        btn.textContent = "Light Mode";
-    } else {
-        btn.textContent = "Dark Mode";
-    }
-});
